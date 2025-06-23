120c331597573691c28d425ff3b8448305ac6c84
SUJAL-7 <awargandsujal@gmail.com>
2025-06-23 17:22:30 +0530

    initial gateway setup

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,25 @@
 import express from 'express';
+import cors from 'cors';
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 
 const app = express();
+
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 
 app.get('/', (req, res) => {
   res.send({ message: 'Hello API' });
 });
 
-app.listen(port, host, () => {
+const server = app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
 });
+
+server.on("error", (err) => {
+  console.log("Server Error :",err);
+}); 
